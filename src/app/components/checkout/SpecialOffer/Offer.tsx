a76b162ffd95951c74e18e014ee791f9f819991c
Guilherme Carvalho <gdsc0301@gmail.com>
2023-09-28 19:03:46 -0300

    Adds structure for the offer template

--- src/app/components/checkout/SpecialOffer/Offer.tsx
+++ src/app/components/checkout/SpecialOffer/Offer.tsx
@@ -1,19 +1,15 @@
 import Image from "next/image";
 
 const Offer = ({dictionary}: {dictionary: any}) => {
+  const dict = dictionary["checkout"];
   return (
     <div className="offer w-full p-10 rounded-[10px] bg-neutral-50">
-      <div className="details grid lg:grid-cols-2">
-        <div className="image">
+      <div className="details grid lg:grid-cols-2 gap-10">
+        <div className="image relative w-full md:h-[591px]">
           <Image src="/images/offer.png" fill alt="Satisfaction 30 days guarantee" />
         </div>
         <div className="content">
-          <h1 className="mb-6">{dictionary["thirdStep"]["offer"]["heading"]["title"]}</h1>
-          <span className="text-2xl">{dictionary["thirdStep"]["offer"]["heading"]["subtitle"]}</span>
-          <p className="mt-6">{dictionary["thirdStep"]["offer"]["content"]}</p>
-          <div className="btn mt-6">
-            <button className="btn-primary">{dictionary["thirdStep"]["offer"]["button"]}</button>
-          </div>
+          <span className="mb-6 text-[32px] text-black" dangerouslySetInnerHTML={{__html: dict["thirdStep"]["offer"]["title"]}}></span>
         </div>
       </div>
     </div>
